b433c6163e7523d50e0e96a8e6cdc61c8fdbc6c3
Oliver <1885435+kokspflanze@users.noreply.github.com>
2023-06-26 14:25:55 +0200

    Update Sitemap.php

--- src/Service/Sitemap.php
+++ src/Service/Sitemap.php
@@ -2,6 +2,7 @@
 
 namespace ZfcSitemap\Service;
 
+use Laminas\Cache\Storage\StorageInterface;
 use Laminas\EventManager\EventManagerAwareInterface;
 use Laminas\EventManager\EventManagerInterface;
 use Laminas\Navigation\AbstractContainer;
@@ -17,15 +18,19 @@ class Sitemap implements EventManagerAwareInterface
     /** @var View\Renderer\RendererInterface */
     protected $renderer;
 
+    /** @var StorageInterface */
+    protected $cache;
+
     /**
      * Sitemap constructor.
      * @param EventManagerInterface $events
      * @param View\Renderer\RendererInterface $renderer
      */
-    public function __construct(EventManagerInterface $events, View\Renderer\RendererInterface $renderer)
+    public function __construct(EventManagerInterface $events, View\Renderer\RendererInterface $renderer, StorageInterface $storage)
     {
         $this->setEventManager($events);
         $this->renderer = $renderer;
+        $this->cache = $storage;
     }
 
     /**
@@ -51,13 +56,17 @@ class Sitemap implements EventManagerAwareInterface
     }
 
     /**
+     * @param string $url
      * @param string|null $containerString
      * @return string
      */
-    public function getSitemap(?string $containerString = null): string
+    public function getSitemap(string $url, ?string $containerString = null): string
     {
-        if (file_exists('./data/zfc-sitemap/sitemap.xml')) {
-            return file_get_contents('./data/zfc-sitemap/sitemap.xml');
+        $fileName = $this->getSitemapCacheKey($url);
+
+        $sitemap = $this->cache->getItem($fileName);
+        if ($sitemap !== null) {
+            return $sitemap;
         }
 
         return $this->getNewSitemap($containerString);
@@ -69,38 +78,44 @@ class Sitemap implements EventManagerAwareInterface
      */
     public function generateSitemapCache(string $url, ?string $containerString = null)
     {
-        $siteMapString = $this->getNewSitemap($containerString);
+        $siteMapString = $this->getNewSitemap($url, $containerString);
+        $url = rtrim($url, '/');
 
         $siteMapString = str_replace(
             '>http://',
             sprintf(
                 '>%s',
-                rtrim($url, '/')
+                str_replace('/sitemap.xml', '', $url)
             ),
             $siteMapString
         );
 
-        if (!is_dir('./data/zfc-sitemap')) {
-            throw new \InvalidArgumentException('"./data/zfc-sitemap" is missing');
-        }
-        $success = file_put_contents('./data/zfc-sitemap/sitemap.xml', $siteMapString);
+        $fileName = $this->getSitemapCacheKey($url);
+        $success = $this->cache->setItem($fileName, $siteMapString);
 
         if (false === $success) {
-            throw new \InvalidArgumentException('could not wirte sitemap in "./data/zfc-sitemap/sitemap.xml", check user write rights');
+            throw new \InvalidArgumentException('could not write sitemap into the cache');
         }
+    }
+
+    protected function getSitemapCacheKey(string $url) :string
+    {
+        $url = rtrim($url, '/');
+
+        return 'zfc-sitemap-' . sha1($url);
     }
 
     /**
      * @param string|null $containerString
      * @return string
      */
-    protected function getNewSitemap(?string $containerString = null): string
+    protected function getNewSitemap(string $url, ?string $containerString = null): string
     {
         /** @var View\Helper\Navigation $navigation */
         $navigation = $this->renderer->navigation($containerString);
         $container = $navigation->getContainer();
 
-        $this->sitemapContainer($container);
+        $this->sitemapContainer($url, $container);
 
         return $navigation->sitemap()
             ->setFormatOutput(true)
@@ -111,8 +126,15 @@ class Sitemap implements EventManagerAwareInterface
      * @param AbstractContainer $container
      * @return \Laminas\EventManager\ResponseCollection
      */
-    protected function sitemapContainer(AbstractContainer $container)
+    protected function sitemapContainer(string $url, AbstractContainer $container)
     {
-        return $this->getEventManager()->trigger(self::EVENT_SITEMAP, $this, ['container' => $container]);
+        return $this->getEventManager()->trigger(
+            self::EVENT_SITEMAP,
+            $this,
+            [
+                'container' => $container,
+                'url' => $url,
+            ]
+        );
     }
 }
